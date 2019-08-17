2a3afaa1bd30e8197cc720e4c6c45b71622169ea
zqzal <936952020@qq.com>
2019-08-17 09:31:01 +0800

    pull 解析方式

--- app/src/main/java/com/xuxi/networktest/MainActivity.java
+++ app/src/main/java/com/xuxi/networktest/MainActivity.java
@@ -3,16 +3,21 @@ package com.xuxi.networktest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.textservice.TextInfo;
 import android.widget.Button;
 import android.widget.TextView;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -22,6 +27,7 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private static final String TAG = "MainActivity";
     TextView responseText;
 
     @Override
@@ -48,6 +54,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+
     private void sendRequestWithOKHttp(){
         new Thread(new Runnable() {
             @Override
@@ -55,17 +62,60 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 try{
                     OkHttpClient client = new OkHttpClient();
                     Request request = new Request.Builder()
-                            .url("https://www.baidu.com")
+                            .url("https://www.bai.com")
                             .build();
                     Response response = client.newCall(request).execute();
                     String responseData = response.body().string();
-                    showResponse(responseData);
+//                    showResponse(responseData);
                 }catch (Exception e){
                     e.printStackTrace();
                 }
             }
         }).start();
     }
+
+    private void parseXMLWithPull(String xmlData){
+        try{
+
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = factory.newPullParser();
+            xmlPullParser.setInput(new StringReader(xmlData));
+            int eventType = xmlPullParser.getEventType();
+            String id = "";
+            String name = "";
+            String version = "";
+            while (eventType != XmlPullParser.END_DOCUMENT){
+                String nodeName = xmlPullParser.getName();
+                switch (eventType){
+                    //开始解析某个节点
+                    case XmlPullParser.START_TAG:
+                        if ("id".equals(nodeName)){
+                            id = xmlPullParser.nextText();
+                        }else if("name".equals(nodeName)){
+                            name = xmlPullParser.nextText();
+                        }else if("version".equals(nodeName)){
+                            version = xmlPullParser.nextText();
+                        }
+                    break;
+                    //完成解析某个节点    
+                    case XmlPullParser.END_TAG:
+                        if ("app".equals(nodeName)){
+                            Log.d(TAG, "id is " + id);
+                            Log.d(TAG, "name is " + name);
+                            Log.d(TAG, "version is " + version);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                eventType = xmlPullParser.next();
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
 
     private void sendRequestWithHttpURLConnection(){
         //开启线程来发起网络请求
